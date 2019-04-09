29ff625f8b297476786db864f0e0d8d7b52d3fdc
mgmdi <14-10444@usb.ve>
2019-04-08 21:29:43 -0400

    agregamos la funcionalidad del slider de fotos

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// SLIDER CON JQUERY
+
+$(document).ready(function(){
+
+
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 1200
+    });
+});
